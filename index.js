ffd795056bc19a82bc159495634be5d2e24d5e52
orndrea <orrndrea@gmail.com>
2024-06-10 23:08:35 -0400

    readme

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ const questions = [{
     name: 'email',
     message: 'What is your email address?',
   },{ type: 'input',
-  username: 'github',
-  message: 'WHEN I enter my GitHub username',} ];
+    name: 'github',
+  message: 'WHEN I enter my GitHub username',} 
+  ,{ type: 'input',
+    name: 'licence',
+    message: 'What is the licence that you use',
+    options: ['MIT', 'BSD 3', 'GPL 3', 'ISC', 'Apache 2.0', 'Unlicense', 'N/A'], } 
+ ,{ type: 'input',
+   name: 'title',
+   message: 'What is the title',}
+   ,{ type: 'input',
+    name: 'description',
+    message: 'What is the description',} 
+    ,{ type: 'input',
+      name: 'installation',
+      message: 'how do you install it',
+      default: 'npm i', } 
+   ,{ type: 'input',
+    name: 'usage',
+    message: 'how did you use the appliance',}
+   ,{ type: 'input',
+    name: 'contribue',
+     message: 'how did you contribute',}
+     ,{ type: 'input',
+      name: 'test',
+      message: 'how do  run test',
+      default: 'npm test' ,},];
+ 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {return return fs.writeFileSync(path.join(process.cwd(), fileName), data)  }
 
 // TODO: Create a function to initialize app
 function init() {}
